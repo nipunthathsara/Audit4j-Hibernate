4b0d9d46cb1def4318078e969440744d01641a20
Janith Bandara <janith3000@gmail.com>
2016-08-07 22:13:10 +0530

    added hibernate persistance module

--- audit4j-hibernate-demo/src/main/java/org/audt4j/demo/spring/config/core/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package org.audt4j.demo.spring.config.core;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
